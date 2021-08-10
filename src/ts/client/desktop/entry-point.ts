44dc49912da4cc3b48231229ec27409a694f0b6e
Stephen Trudel <github@strisys.com>
2021-08-10 08:45:34 -0400

    clean up

--- src/ts/client/desktop/entry-point.ts
+++ src/ts/client/desktop/entry-point.ts
@@ -1,10 +1,8 @@
 import { app } from 'electron';
 import { createLogger } from './util/logger';
-import { start } from './util/hot-reload';
 import { init as initDesktop } from './views/shell/main';
 
 const logger = createLogger('entry-point');
-start();
 
 const initialize = async () => {
   logger.info('ready: starting application ...');
